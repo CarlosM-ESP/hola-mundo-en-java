7ad06f2df5978800d68202cb75db6bf654287204
CarlosM-DAW2019 <seginf2018@gmail.com>
2020-09-30 19:05:03 +0200

    Pruebas tags

--- holaMundo.java
+++ holaMundo.java
@@ -2,5 +2,6 @@ public class holaMundo{
 	public static void main(String[] Args){
 		//Pruebas modificacion para git
 		System.out.println("Hola, Mundo");
+		System.out.println("Pruebas con Git");
 	}
 }
